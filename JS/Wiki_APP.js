d2ae936f80f7ae2de760cfbb2e21b42630250782
James Bint <khakimonk@gmail.com>
2016-05-17 10:08:10 +0100

    working button

--- JS/Wiki_APP.js
+++ JS/Wiki_APP.js
@@ -1,3 +1,10 @@
-function showInput () {
-  var inputBox = document.getElementById('userSearch');
-};
+var $inputBox = $('#userSearch');
+
+var $searchToggle = $('#searchToggle');
+
+$(document).ready(function() {
+  $inputBox.animate({width: "toggle", opacity: "toggle"}, 800).removeClass('showInput');
+   $searchToggle.click(function(event) {
+    $inputBox.animate({width: "toggle", opacity: "toggle"}, 800).addClass('showInput');
+  });
+});
